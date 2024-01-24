d47339fca9ee66aea56712d4d7d73be0ed8308fa
kei ito <ito.kei@jvckenwood.com>
2024-01-24 09:38:47 +0900

    first commit

---
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //ExcelBook book = new();
+
+            //book.SetAAAEntity("AAA Entity");
+            //book.SetBBBEntity("BBB Entity");
+
+            //var saveDir = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            //var fileName = "ExcelTest.xlsx";
+            //var savePath = System.IO.Path.Combine(saveDir, fileName);
+            //book.Save(savePath);
+        }
+    }
+}
